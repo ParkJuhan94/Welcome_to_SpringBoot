5672202ccf10882c028449e730a75127cb2d9561
JuHan <1004sunye@naver.com>
2022-06-23 21:14:47 +0900

    0623

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -1,4 +1,18 @@
 package hello.hellospring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class helloController {
+
+    //컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다.
+    //스프링 부트 템플릿엔진 기본 viewName 매핑
+    //resources:templates/ +{ViewName}+ .html
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
 }
