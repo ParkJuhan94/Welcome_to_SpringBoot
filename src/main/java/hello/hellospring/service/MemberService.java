cba81330198c767ab926e5a42572f4503b133c7a
JuHan <1004sunye@naver.com>
2022-07-05 13:47:21 +0900

    0705

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,7 +14,9 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-
+    //  @Autowired
+    //참고: 생성자에 @Autowired 를 사용하면 객체 생성 시점에 스프링 컨테이너에서 해당 스프링 빈을 찾아서
+    //주입한다. 생성자가 1개만 있으면 @Autowired 는 생략할 수 있다.
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
